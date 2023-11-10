4f2596552712d0b91508483977568c18f1ed24f4
kone <corazonkk2021@gmail.com>
2023-11-10 21:30:52 +0700

    kone p1

---
+++ app/src/main/java/com/team8/socialmedia/kone/ChatActivity.java
@@ -0,0 +1,16 @@
+package com.team8.socialmedia.kone;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.team8.socialmedia.R;
+
+public class ChatActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat);
+    }
+}
