6e4e32de9eaecf5d2d0566f54f437d62cd5fd2ce
kone <corazonkk2021@gmail.com>
2023-11-14 23:30:10 +0700

    srum 12

--- app/src/main/java/com/team8/socialmedia/kone/ChatActivity.java
+++ app/src/main/java/com/team8/socialmedia/kone/ChatActivity.java
@@ -3,17 +3,20 @@ package com.team8.socialmedia.kone;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -29,24 +32,35 @@ import com.squareup.picasso.Picasso;
 import com.team8.socialmedia.MainActivity;
 import com.team8.socialmedia.R;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 public class ChatActivity extends AppCompatActivity {
-//  View from xml
+    //  View from xml
     Toolbar toolbar;
     RecyclerView recyclerView;
     ImageView profileIv;
     TextView nameTv, userStatusTv;
     EditText messageEt;
     ImageButton sendBtn;
-//    Firebase auth
+    //    Firebase auth
     FirebaseAuth firebaseAuth;
     FirebaseDatabase firebaseDatabase;
     DatabaseReference userDbRef;
 
+    //For checking if use has seen message or not
+    ValueEventListener seenListener;
+    DatabaseReference userRefForSeen;
+    List<ModelChat> chatList;
+    AdapterChat adapterChat;
+
 
     String hisUid;
     String myUid;
+    String hisImage;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -60,34 +74,48 @@ public class ChatActivity extends AppCompatActivity {
         profileIv = findViewById(R.id.chat_profileIv);
         nameTv = findViewById(R.id.chat_nameTv);
         userStatusTv = findViewById(R.id.chat_userStatusTv);
-        messageEt =findViewById(R.id.chat_messageEt);
+        messageEt = findViewById(R.id.chat_messageEt);
         sendBtn = findViewById(R.id.chat_sendBtn);
+
+        //Layout (LinearLayout) for RecyclerView
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        linearLayoutManager.setStackFromEnd(true);
+
+        //Recyclerview properties
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(linearLayoutManager);
+
+        /*          On Clicking user from users list we have passed that user's UID using intent
+         *          So get that uid here to get the profile picture, name and start chat with that user   */
+        Intent intent = getIntent();
+        hisUid = intent.getStringExtra("hisUid");
 
 //      Firebase auth instance
         firebaseAuth = FirebaseAuth.getInstance();
 
         firebaseDatabase = FirebaseDatabase.getInstance();
-        userDbRef = firebaseDatabase.getReference();
+        userDbRef = firebaseDatabase.getReference("Users");
 
         // Search user to get that user's info
         Query userQuery = userDbRef.orderByChild("uid").equalTo(hisUid);
         // get user picture and name
-        userQuery.addValueEventListener(new ValueEventListener(){
+        userQuery.addValueEventListener(new ValueEventListener() {
 
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
                 // Check until required info is recived
-                for (DataSnapshot ds: dataSnapshot.getChildren()){
+                for (DataSnapshot ds : dataSnapshot.getChildren()) {
+                    //get data
                     String name = "" + ds.child("name").getValue();
-                    String image = "" + ds.child("image").getValue();
+                    hisImage = "" + ds.child("image").getValue();
 
                     // Set data
                     nameTv.setText(name);
-                    try{
+
+                    try {
                         // image received, set it to imageview in toolbar
-                        Picasso.get().load(image).placeholder(R.drawable.ic_default).into(profileIv);
-                    }
-                    catch (Exception e){
+                        Picasso.get().load(hisImage).placeholder(R.drawable.ic_default).into(profileIv);
+                    } catch (Exception e) {
                         // There is excreption getting picture, set default picture
                         Picasso.get().load(R.drawable.ic_default).into(profileIv);
                     }
@@ -101,13 +129,13 @@ public class ChatActivity extends AppCompatActivity {
             }
         });
         // Click button to send message
-        sendBtn.setOnClickListener(new View.OnClickListener(){
+        sendBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 //get text from edit text
                 String message = messageEt.getText().toString().trim();
                 //Check if text is empty or not
-                if(TextUtils.isEmpty(message)){
+                if (TextUtils.isEmpty(message)) {
                     //text empty
                     Toast.makeText(ChatActivity.this, "Can't send the empty message...", Toast.LENGTH_SHORT).show();
                 }
@@ -118,27 +146,79 @@ public class ChatActivity extends AppCompatActivity {
             }
         });
 
+        readMessages();
 
-/*          On Clicking user from users list we have passed that user's UID using intent
- *          So get that uid here to get the profile picture, name and start chat with that user   */
-        Intent intent = getIntent();
-        hisUid = intent.getStringExtra("hisUid");
+        seenMessage();
+    }
+
+    private void seenMessage() {
+        userRefForSeen = FirebaseDatabase.getInstance().getReference("Chats");
+        seenListener = userRefForSeen.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot datasnapshot) {
+                for (DataSnapshot ds : datasnapshot.getChildren()) {
+                    ModelChat chat = ds.getValue(ModelChat.class);
+                    if (chat.getReceiver().equals(myUid) && chat.getSender().equals(hisUid)) {
+                        HashMap<String, Object> hasSeenHasMap = new HashMap<>();
+                        hasSeenHasMap.put("isSeen", true);
+                        ds.getRef().updateChildren(hasSeenHasMap);
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
+
+    private void readMessages() {
+        chatList = new ArrayList<>();
+        DatabaseReference dbRef = FirebaseDatabase.getInstance().getReference("Chats");
+        dbRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                chatList.clear();
+                for (DataSnapshot ds : dataSnapshot.getChildren()) {
+                    ModelChat chat = ds.getValue(ModelChat.class);
+                    if (chat.getReceiver().equals(myUid) && chat.getSender().equals(hisUid) ||
+                            chat.getReceiver().equals(hisUid) && chat.getSender().equals(myUid)) {
+                        chatList.add(chat);
+                    }
+                    //Adapter
+                    adapterChat = new AdapterChat(ChatActivity.this, chatList, hisImage);
+                    adapterChat.notifyDataSetChanged();
+                    //set adapter to recyclerview
+                    recyclerView.setAdapter(adapterChat);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
     }
 
     private void sendMessage(String message) {
         /*"Chats" node will be created that will contain all chats
-        * Whenever user sands message it will create new child in "chats" node nad that child will contain
-        * the following key values
-        * sender: UID of sender
-        * receiver: UID if receiver
-        * message: the actual message */
+         * Whenever user sands message it will create new child in "chats" node nad that child will contain
+         * the following key values
+         * sender: UID of sender
+         * receiver: UID if receiver
+         * message: the actual message */
         DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference();
+
+        String timestamp = String.valueOf(System.currentTimeMillis());
 
         HashMap<String, Object> hashMap = new HashMap<>();
         hashMap.put("sender", myUid);
         hashMap.put("receiver", hisUid);
         hashMap.put("message", message);
-        databaseReference.child("Chat").push().setValue(hashMap);
+        hashMap.put("timestamp", timestamp);
+        hashMap.put("isSeen", false);
+        databaseReference.child("Chats").push().setValue(hashMap);
         //Reset edittext after sending message
         messageEt.setText("");
     }
@@ -161,6 +241,12 @@ public class ChatActivity extends AppCompatActivity {
     protected void onStart() {
         checkUserStatus();
         super.onStart();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        userRefForSeen.removeEventListener(seenListener);
     }
 
     @Override
