69e80d595d51f7ea008edc8f64b0ddef24c6cffd
Hungg <Tienhungg0305@gmail.com>
2023-12-10 19:35:08 +0700

    Revert "OK"
    This reverts commit 910c2f33991ce3783a40ce98e968abea61f695ce.

--- app/src/main/java/com/team8/socialmedia/vu/AdapterPosts.java
+++ app/src/main/java/com/team8/socialmedia/vu/AdapterPosts.java
@@ -1,6 +1,5 @@
 package com.team8.socialmedia.vu;
 
-import android.annotation.SuppressLint;
 import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
@@ -60,7 +59,7 @@ public class AdapterPosts extends RecyclerView.Adapter<AdapterPosts.MyHolder> {
     Context context;
     List<ModelPost> postList;
     String myUid;
-    boolean mProcessLike = false;
+    boolean mProcessLike =false;
     private DatabaseReference likesRef;
     private DatabaseReference postsRef;
 
@@ -80,7 +79,6 @@ public class AdapterPosts extends RecyclerView.Adapter<AdapterPosts.MyHolder> {
         return new MyHolder(view);
     }
 
-    @SuppressLint("RecyclerView")
     @Override
     public void onBindViewHolder(@androidx.annotation.NonNull MyHolder holder, int position) {
         String uid = postList.get(position).getUid();
@@ -105,11 +103,11 @@ public class AdapterPosts extends RecyclerView.Adapter<AdapterPosts.MyHolder> {
         holder.pTimeTv.setText(pTime);
         holder.pTitleTv.setText(pTitle);
         holder.pDescriptionTv.setText(pDescription);
-        holder.pLikesTv.setText(pLikes + " Likes");
-        holder.pCommentsTv.setText(pComments + " Comments");
+        holder.pLikesTv.setText(pLikes+" Likes");
+        holder.pCommentsTv.setText(pComments+" Comments");
 
 
-        setLikes(holder, pId);
+        setLikes(holder,pId);
         try {
             Picasso.get().load(uDp).placeholder(R.drawable.ic_face_light).into(holder.uPictureIv);
         } catch (Exception e) {
@@ -136,12 +134,16 @@ public class AdapterPosts extends RecyclerView.Adapter<AdapterPosts.MyHolder> {
         holder.likeBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                int pLikes = Integer.parseInt(postList.get(position).getpLikes());
+                //get total number of likes for the post, whose like button clicked
+                //if currently signed in user has not liked it before
+                //increase value by i, otherwise decrease value by 1
+                final int pLikes = Integer.parseInt(postList.get(position).getpLikes());
                 mProcessLike = true;
-                String postIde = postList.get(position).getpId();
+                //get id of the post clicked
+                final String postIde=postList.get(position).getpId();
                 likesRef.addValueEventListener(new ValueEventListener() {
                     @Override
-                    public void onDataChange(@androidx.annotation.NonNull DataSnapshot snapshot) {
+                    public void onDataChange(@androidx.annotation.NonNull @NotNull DataSnapshot snapshot) {
                         if (mProcessLike) {
                             if (snapshot.child(postIde).hasChild(myUid)) {
                                 postsRef.child(postIde).child("pLikes").setValue("" + (pLikes - 1));
@@ -156,12 +158,13 @@ public class AdapterPosts extends RecyclerView.Adapter<AdapterPosts.MyHolder> {
                     }
 
                     @Override
-                    public void onCancelled(@androidx.annotation.NonNull DatabaseError error) {
+                    public void onCancelled(@androidx.annotation.NonNull @NotNull DatabaseError error) {
 
                     }
                 });
             }
         });
+
         holder.shareBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -187,8 +190,8 @@ public class AdapterPosts extends RecyclerView.Adapter<AdapterPosts.MyHolder> {
             @Override
             public void onClick(View v) {
                 //start PostDetailActivity
-                Intent intent = new Intent(context, PostDetailActivity.class);
-                intent.putExtra("postId", pId);//will get detail of post using this id, its id of posts
+                Intent intent=new Intent(context, PostDetailActivity.class);
+                intent.putExtra("postId",pId);//will get detail of post using this id, its id of posts
                 context.startActivity(intent);
             }
         });
@@ -263,17 +266,16 @@ public class AdapterPosts extends RecyclerView.Adapter<AdapterPosts.MyHolder> {
         likesRef.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@androidx.annotation.NonNull @NotNull DataSnapshot snapshot) {
-                if (snapshot.child(postKey).hasChild(myUid)) {
+                if(snapshot.child(postKey).hasChild(myUid)){
                     //user has liked this post
                     holder.likeBtn.setCompoundDrawablesWithIntrinsicBounds(R.drawable.ic_liked, 0, 0, 0);
                     holder.likeBtn.setText("Liked");
-                } else {
+                }else {
                     //user has not liked this post
                     holder.likeBtn.setCompoundDrawablesWithIntrinsicBounds(R.drawable.ic_like_black, 0, 0, 0);
                     holder.likeBtn.setText("Like");
                 }
             }
-
             @Override
             public void onCancelled(@androidx.annotation.NonNull @NotNull DatabaseError error) {
 
@@ -293,7 +295,7 @@ public class AdapterPosts extends RecyclerView.Adapter<AdapterPosts.MyHolder> {
             popupMenu.getMenu().add(Menu.NONE, 0, 0, s);
             popupMenu.getMenu().add(Menu.NONE, 1, 0, s1);
         }
-        popupMenu.getMenu().add(Menu.NONE, 2, 0, "View Detail");
+        popupMenu.getMenu().add(Menu.NONE,2,0,"View Detail");
 
         popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
             @Override
@@ -307,9 +309,9 @@ public class AdapterPosts extends RecyclerView.Adapter<AdapterPosts.MyHolder> {
                     intent.putExtra("key", "editPost");
                     intent.putExtra("editPostId", pId);
                     context.startActivity(intent);
-                } else if (id == 2) {
-                    Intent intent = new Intent(context, PostDetailActivity.class);
-                    intent.putExtra("postId", pId);//will get detail of post using this id, its id of posts
+                }else if (id==2){
+                    Intent intent=new Intent(context, PostDetailActivity.class);
+                    intent.putExtra("postId",pId);//will get detail of post using this id, its id of posts
                     context.startActivity(intent);
                 }
 
